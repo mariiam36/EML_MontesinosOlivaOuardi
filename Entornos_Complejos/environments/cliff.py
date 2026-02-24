55d05d36dbc67ef30eaffc54a5ccf3ef7251a91a
montesinos3 <jormonbra@gmail.com>
2026-02-24 12:37:05 +0100

    ejecuciones varias del main y prueba de alfa fijo

--- Entornos_Complejos/environments/cliff.py
+++ Entornos_Complejos/environments/cliff.py
@@ -1,7 +1,7 @@
 import gymnasium as gym
 
-def make_cliff_env(render_mode="ansi", seed=None):
-    env = gym.make("CliffWalking-v1", render_mode=render_mode)
+def make_cliff_env(render_mode="ansi", seed=None, steps=-1):
+    env = gym.make("CliffWalking-v1", render_mode=render_mode, max_episode_steps=steps)
     if seed is not None:
         env.reset(seed=seed)
     return env
